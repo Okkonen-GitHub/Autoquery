abdb06e0beb41f270930c641b24da7225b150f80
Okkonen <okko.oikkonen@gmail.com>
2021-12-25 13:49:10 +0200

    rename component

--- query.js
+++ query.js
@@ -1,12 +1,12 @@
 
 const get_syllable = () => {
   let s = document.querySelector("body > div.main.page > div.middle > div.canvasArea > div.round > div.syllable");
-  return s ? s.innerHTML.toUpperCase() : console.log("Shits fucked");
+  return s ? s.innerHTML.toUpperCase() : console.log("Shits fucked [CAN'T FIND THE SYLLABLE]");
 }
 
 const get_turn = () => {
   let turn = document.querySelector("body > div.main.page > div.bottom > div.round > div.selfTurn");
-  if (turn.getAttribute("hidden") === null) {
+  if (turn.getAttribute("hidden") === null) { // check if it's user's turn
     return true;
   } else {
     return false;
@@ -23,11 +23,11 @@ const main = async () => {
   
   let syllable = get_syllable()
   setInterval(() => {
-    let temp = get_syllable()
+    let temp = get_syllable() // if the syllable has changed and it's users turn, then query
     if (syllable !== temp && get_turn()) {
       syllable = temp;
       console.log(syllable);
-      window.open(
+      let query = window.open(
         `https://www.thefreedictionary.com/words-containing-${syllable}`,
         'query',
         'width=400, height=400, toolbar=0, menubar=0, scrollbars=0, resizable=1, location=0'
@@ -35,5 +35,5 @@ const main = async () => {
     }
   }, 1000);
 }
-main();
 
+main();
